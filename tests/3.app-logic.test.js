f161b1b65e8dc7507af91c26163cafb29024ef97
mabrey64 <mabreydn@mail.uc.edu>
2022-09-21 12:12:24 -0400

    Added methods to index.js
    Methods and parameters were added to pass index.js tests.

--- tests/3.app-logic.test.js
+++ tests/3.app-logic.test.js
@@ -9,25 +9,25 @@ const path = require(`path`);
 const html = fs.readFileSync(path.resolve(__dirname, `../index.html`), `utf8`);
 jest.dontMock(`fs`);
 
-// const rewire = require(`rewire`);
-// const updateMessagesMock = rewire(`../resources/js/index`).__get__(`updateMessages`);
-// const fetchMessagesMock = rewire(`../resources/js/index`).__get__(`fetchMessages`);
-// const formatMessageMock = rewire(`../resources/js/index`).__get__(`formatMessage`);
+const rewire = require(`rewire`);
+const updateMessagesMock = rewire(`../resources/js/index`).__get__(`updateMessages`);
+const fetchMessagesMock = rewire(`../resources/js/index`).__get__(`fetchMessages`);
+const formatMessageMock = rewire(`../resources/js/index`).__get__(`formatMessage`);
 
 
 describe(`Chatting Logic`, function () {
   beforeEach(() => {
     document.documentElement.innerHTML = html.toString();
   });
-  
+
   afterEach(() => {
     // restore the original func after test
     jest.resetModules();
   });
 
-  it.skip(`works`, function() { // TODO: Fix those tests
-    // updateMessagesMock();
-    // expect(fetchMessagesMock).toBeCalled();
-    // expect(formatMessageMock).toBeCalled();
+  it.skip(`works`, function () { // TODO: Fix those tests
+    updateMessagesMock();
+    expect(fetchMessagesMock).toBeCalled();
+    expect(formatMessageMock).toBeCalled();
   });
 });
